42a6e0f34547e4aa1e67c968adf56711419836d9
sara-dsn <142814795+sara-dsn@users.noreply.github.com>
2023-09-11 18:32:05 +0200

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,8 +1,9 @@
 var n=1;
 var pre=window.prompt("Entrez le prénom N°"+n+" ou laissez le champ vide pour arrêter la saisie");
-console.log("prénom"+n+"est"+pre)
+console.log("prénom"+n+"est"+pre);
 while (pre!=0)
 {
 n++;
 pre=window.prompt("Entrez le prénom N°"+n+" ou laissez le champ vide pour arrêter la saisie");
+  console.log("prénom"+n+"est"+pre);
 }
